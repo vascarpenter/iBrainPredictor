03ebdccc1bcfa40e60d68ace3eb5ebc0e5a0800b
vascarpenter <gikoha5666@gmail.com>
2018-09-24 12:24:34 +0900

    Initial Commit

---
+++ iBrainPredictor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBrainPredictor
+//
+//  Created by Namikare Gikoha on 2018/09/24.
+//  Copyright © 2018年 gikoha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
